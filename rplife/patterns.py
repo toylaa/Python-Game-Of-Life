a9816faf9e2266c2a59ff090ca8ffacde6760e90
Sir Toylaa Montgomery <tcmcd1337@gmail.com>
2023-12-04 16:51:46 -0500

    from_toml() function

--- rplife/patterns.py
+++ rplife/patterns.py
@@ -1,6 +1,35 @@
 from dataclasses import dataclass
 
+
+
+## To parse a TOML file and read its content into Python objects,
+#  if you’re using Python 3.11 or later, you can use the standard-library module tomllib . 
+# Otherwise, you should use the third-party library tomli, which is compatible with tomllib.
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomllib
+
+
 @dataclass
 class Pattern: 
     name: str 
     alive_cells: set[tuple[int, int]]
+
+
+    ## Class methods are great when you need to provide an alternative
+    #   constructor in a class. 
+    ## These types of methods receive the current class as their first argument, 
+    #   "cls" .
+    @classmethod
+    def from_toml(cls, name, toml_data):
+        ## Create and return an instance of the class using the cls argument. 
+        return cls(
+            name,
+            alive_cells = {tuple(cell) for cell in toml_data["alive_cells"]},            
+        )
+    ## Create a set of tuples from the list of lists that you get from the TOML file. 
+            ## Each tuple will contain the coordinates of a living cell on the life grid.
+            ## Note that to access the alive cells in the TOML data,
+            #   you can use the dictionary lookup notation with the name  
+            #    of the target key in square brackets.
