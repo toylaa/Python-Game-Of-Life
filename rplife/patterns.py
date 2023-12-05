6109ea6b0c62032cb2f18352851b34a22913c82c
Sir Toylaa Montgomery <tcmcd1337@gmail.com>
2023-12-05 10:09:42 -0500

    toml pattern functions
    also updated python/venv setup to 3.12 for tomllib

--- rplife/patterns.py
+++ rplife/patterns.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
+from pathlib import Path
 
-
+PATTERNS_FILE = Path(__file__).parent / "patterns.toml"
 
 ## To parse a TOML file and read its content into Python objects,
 #  if you’re using Python 3.11 or later, you can use the standard-library module tomllib . 
@@ -33,3 +34,14 @@ class Pattern:
             ## Note that to access the alive cells in the TOML data,
             #   you can use the dictionary lookup notation with the name  
             #    of the target key in square brackets.
+
+    
+def get_pattern(name, filename=PATTERNS_FILE):
+    data = tomllib.loads(filename.read_text(encoding="utf-8"))
+    return Pattern.from_toml(name, toml_data=data[name])
+
+def get_all_patterns(filename=PATTERNS_FILE):
+    data = tomllib.loads(filename.read_text(encoding="utf-8"))
+    return [
+        Pattern.from_toml(name, toml_data) for name, toml_data in data.items()
+    ]
--- rplife/test1.py
+++ rplife/test1.py
@@ -8,22 +8,40 @@ blinker = patterns.Pattern("Blinker", {(2,1), (2,2), (2,3)})
 ## Next up, you create a LifeGrid object.
 grid = grid.LifeGrid(blinker)
 ## Note that when you print this object, you get the pattern’s name and live cells.
-print(grid)
-# Blinker:
-# Alive Cells --> [(2, 1), (2, 2), (2, 3)]
+print(grid.as_string((0, 0, 5, 5)))
+"""  
+Blinker  
+‧ ‧ ‧ ‧ ‧ 
+‧ ‧ ‧ ‧ ‧ 
+‧ ♥ ♥ ♥ ‧ 
+‧ ‧ ‧ ‧ ‧ 
+‧ ‧ ‧ ‧ ‧  
+"""
 
 ## Evolve the grid by calling .evolve().
 grid.evolve()
-print(grid)
+print(grid.as_string((0, 0, 5, 5)))
 ##  This time, you get a different set of living cells.
-# Blinker:
-# Alive Cells --> [(1, 2), (2, 2), (3, 2)]
+"""
+ Blinker  
+‧ ‧ ‧ ‧ ‧ 
+‧ ‧ ♥ ‧ ‧ 
+‧ ‧ ♥ ‧ ‧ 
+‧ ‧ ♥ ‧ ‧ 
+‧ ‧ ‧ ‧ ‧ 
+"""
 
 
 ## If you evolve the grid again, then you get the same set of live cells
 #   that you use as the initial seed for the game. 
 ## This is because the Blinker pattern is an oscillator pattern 
 grid.evolve()
-print(grid)
-# Blinker:
-# Alive Cells --> [(2, 1), (2, 2), (2, 3)]
+print(grid.as_string((0, 0, 5, 5)))
+""" 
+ Blinker
+‧ ‧ ‧ ‧ ‧
+‧ ‧ ‧ ‧ ‧
+‧ ♥ ♥ ♥ ‧
+‧ ‧ ‧ ‧ ‧
+‧ ‧ ‧ ‧ ‧ 
+"""
